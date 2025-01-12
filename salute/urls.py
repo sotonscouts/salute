ecf98e2b43b81a642d00bb38278ce4c6ae9de85d
Dan Trickey <git@trickey.io>
2025-01-12 18:00:51 +0000

    chore: add branding to admin site

--- salute/urls.py
+++ salute/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
+admin.site.site_title = "Salute"
+admin.site.site_header = "Salute Backend"
+admin.site.index_title = "System administration"
+
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("salute-backend/", admin.site.urls),
 ]
