6fda2c9d255db4715d615e5a0d8884532806967f
Dan Trickey <git@trickey.io>
2025-02-07 20:45:53 +0000

    feat: Add group query to fetch single group by ID

--- salute/hierarchy/graphql/schema.py
+++ salute/hierarchy/graphql/schema.py
@@ -5,7 +5,7 @@ from strawberry_django.permissions import IsAuthenticated
 from salute.hierarchy import models as hierarchy_models
 from salute.hierarchy.constants import SECTION_TYPE_INFO
 
-from .graph_types import District, SectionTypeInfo
+from .graph_types import District, Group, SectionTypeInfo
 
 
 @sb.type
@@ -13,6 +13,10 @@ class HierarchyQuery:
     @sd.field(description="Get the district", extensions=[IsAuthenticated()])
     def district(self, info: sb.Info) -> District:
         return hierarchy_models.District.objects.get()  # type: ignore[return-value]
+
+    @sd.field(description="Get a group by ID", extensions=[IsAuthenticated()])
+    def group(self, group_id: sb.relay.GlobalID, info: sb.Info) -> Group:
+        return hierarchy_models.Group.objects.get(id=group_id.node_id)  # type: ignore[return-value]
 
     @sb.field(
         description="Get all possible section types",
---
+++ salute/hierarchy/tests/graphql/test_group_query.py
@@ -0,0 +1,173 @@
+from uuid import UUID
+
+import pytest
+from django.urls import reverse
+from strawberry.relay import to_base64
+from strawberry_django.test.client import Response, TestClient
+
+from salute.accounts.models import User
+from salute.hierarchy.factories import GroupFactory, GroupSectionFactory
+
+
+@pytest.mark.django_db
+class TestGroupQuery:
+    url = reverse("graphql")
+
+    QUERY = """
+    query getGroup($groupId: GlobalID!) {
+        group(groupId: $groupId) {
+            id
+            unitName
+            shortcode
+            displayName
+            charityNumber
+            ordinal
+            district {
+                unitName
+            }
+        }
+    }
+    """
+
+    def test_query__not_authenticated(self) -> None:
+        group = GroupFactory()
+        group_id = to_base64("Group", group.id)
+        client = TestClient(self.url)
+        results = client.query(
+            self.QUERY,
+            variables={"groupId": group_id},  # type: ignore[dict-item]
+            assert_no_errors=False,
+        )
+
+        assert isinstance(results, Response)
+
+        assert results.errors == [
+            {"message": "User is not authenticated.", "locations": [{"line": 3, "column": 9}], "path": ["group"]}
+        ]
+        assert results.data is None
+
+    def test_query__no_group(self, admin_user: User) -> None:
+        """This should not happen in production. There must always be exactly one district."""
+        group_id = to_base64("Group", UUID(int=0))
+        client = TestClient(self.url)
+        with client.login(admin_user):
+            result = client.query(
+                self.QUERY,
+                variables={"groupId": group_id},  # type: ignore[dict-item]
+                assert_no_errors=False,
+            )
+
+        assert isinstance(result, Response)
+
+        assert result.data is None
+        assert result.errors == [
+            {
+                "locations": [{"column": 9, "line": 3}],
+                "message": "Group matching query does not exist.",
+                "path": ["group"],
+            }
+        ]
+
+    def test_query(self, admin_user: User) -> None:
+        group = GroupFactory()
+        group_id = to_base64("Group", group.id)
+        client = TestClient(self.url)
+        with client.login(admin_user):
+            result = client.query(
+                self.QUERY,
+                variables={"groupId": group_id},  # type: ignore[dict-item]
+                assert_no_errors=False,
+            )
+
+        assert isinstance(result, Response)
+
+        assert result.errors is None
+        assert result.data == {
+            "group": {
+                "id": group_id,
+                "displayName": group.display_name,
+                "shortcode": group.shortcode,
+                "unitName": group.unit_name,
+                "charityNumber": group.charity_number,
+                "district": {
+                    "unitName": group.district.unit_name,
+                },
+                "ordinal": group.ordinal,
+            }
+        }
+
+
+class TestGroupJoinSectionsQuery:
+    url = reverse("graphql")
+
+    QUERY = """
+    query GroupWithSections($groupId: GlobalID!) {
+        group(groupId: $groupId) {
+            shortcode
+            sections {
+                edges {
+                    node {
+                        __typename
+                        unitName
+                    }
+                }
+                totalCount
+            }
+        }
+    }
+    """
+
+    def test_query_sections__none(self, admin_user: User) -> None:
+        group = GroupFactory()
+        group_id = to_base64("Group", group.id)
+        client = TestClient(self.url)
+        with client.login(admin_user):
+            result = client.query(
+                self.QUERY,
+                variables={"groupId": group_id},  # type: ignore[dict-item]
+            )
+
+        assert isinstance(result, Response)
+
+        assert result.errors is None
+        assert result.data == {
+            "group": {
+                "shortcode": group.shortcode,
+                "sections": {
+                    "edges": [],
+                    "totalCount": 0,
+                },
+            }
+        }
+
+    def test_query_sections(self, admin_user: User) -> None:
+        group = GroupFactory()
+        group_id = to_base64("Group", group.id)
+        sections = GroupSectionFactory.create_batch(size=5, group=group)
+        client = TestClient(self.url)
+        with client.login(admin_user):
+            result = client.query(
+                self.QUERY,
+                variables={"groupId": group_id},  # type: ignore[dict-item]
+            )
+
+        assert isinstance(result, Response)
+
+        assert result.errors is None
+        assert result.data == {
+            "group": {
+                "shortcode": group.shortcode,
+                "sections": {
+                    "edges": [
+                        {
+                            "node": {
+                                "__typename": "GroupSection",  # Specific type: no district field
+                                "unitName": s.unit_name,
+                            }
+                        }
+                        for s in sorted(sections, key=lambda s: s.id)  # Default sort
+                    ],
+                    "totalCount": 5,
+                },
+            }
+        }
