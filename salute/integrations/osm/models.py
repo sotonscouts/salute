c50da435e05ff1119b0181ca9b6c5128f732e1a6
Dan Trickey <git@trickey.io>
2025-08-18 21:20:04 +0100

    feat: add OSM models

---
+++ salute/integrations/osm/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+from salute.core.models import BaseModel
+from salute.hierarchy.models import Section
+
+
+class OSMSyncLog(BaseModel):
+    date = models.DateField()
+    data = models.JSONField()
+    error = models.TextField(blank=True)
+    success = models.BooleanField()
+
+    def __str__(self) -> str:
+        return f"{self.date} - {self.success}"
+
+    class Meta:
+        ordering = ("-date",)
+
+
+class OSMSectionHeadcountRecord(BaseModel):
+    section = models.ForeignKey(Section, on_delete=models.CASCADE)
+    date = models.DateField()
+
+    sync_log = models.ForeignKey(OSMSyncLog, on_delete=models.SET_NULL, null=True)
+    young_person_count = models.IntegerField()
+
+    class Meta:
+        ordering = ("date",)
+        constraints = [
+            models.UniqueConstraint(fields=["section", "date"], name="unique_section_date")
+        ]
+
+    def __str__(self) -> str:
+        return f"{self.section.display_name} - {self.date} - {self.young_person_count}"
