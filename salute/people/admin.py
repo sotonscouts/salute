6d471f286ffd80438af3a133e8d68fce3f274cf1
Dan Trickey <git@trickey.io>
2026-05-13 22:43:17 +0100

    feat: add mailing groups for permit holders

--- salute/people/admin.py
+++ salute/people/admin.py
@@ -3,10 +3,19 @@ from django.db import models
 from django.http import HttpRequest
 
 from salute.core.admin import BaseModelAdminMixin
+from salute.core.models import BaseModel
 from salute.integrations.tsa.admin import TSAObjectModelAdminMixin
 from salute.integrations.tsa.models import TSATimestampedObject
 from salute.mailing_groups.models import SystemMailingGroupMembership
-from salute.people.models import Permit, PermitActivity, PermitCategory, PermitStatus, PermitType, Person
+from salute.people.models import (
+    Permit,
+    PermitActivity,
+    PermitActivityGroup,
+    PermitCategory,
+    PermitStatus,
+    PermitType,
+    Person,
+)
 from salute.roles.models import Accreditation, Role
 
 
@@ -80,12 +89,33 @@ class PersonAdmin(TSAObjectModelAdminMixin, admin.ModelAdmin):
         return super().get_readonly_fields(request, obj) + ["contact_email", "workspace_account"]
 
 
+@admin.register(PermitActivityGroup)
+class PermitActivityGroupAdmin(BaseModelAdminMixin, admin.ModelAdmin):
+    list_display = ("name", "mailing_slug")
+    search_fields = ("name", "mailing_slug")
+
+    fieldsets = ((None, {"fields": ("name", "mailing_slug")}),) + BaseModelAdminMixin.FIELDSETS
+
+    def has_change_permission(self, request: HttpRequest, obj: models.Model | None = None) -> bool:
+        return request.user.is_superuser
+
+    def has_add_permission(self, request: HttpRequest, obj: models.Model | None = None) -> bool:
+        return request.user.is_superuser
+
+
 @admin.register(PermitActivity)
 class PermitActivityAdmin(BaseModelAdminMixin, admin.ModelAdmin):
-    list_display = ("name",)
-    search_fields = ("name",)
+    list_display = ("name", "group")
+    list_filter = ("group",)
+    search_fields = ("name", "group__name")
 
-    fieldsets = ((None, {"fields": ("name",)}),) + BaseModelAdminMixin.FIELDSETS
+    fieldsets = ((None, {"fields": ("name", "group")}),) + BaseModelAdminMixin.FIELDSETS
+
+    def get_readonly_fields(self, request: HttpRequest, obj: BaseModel | None = None) -> list[str]:
+        return super().get_readonly_fields(request, obj) + ["name"]
+
+    def has_change_permission(self, request: HttpRequest, obj: models.Model | None = None) -> bool:
+        return request.user.is_superuser
 
 
 @admin.register(PermitCategory)
